a8313259776515fb304dea2f1a934380bb90b6e7
BenThere <benjaminbirdsall@icloud.com>
2023-09-14 14:30:27 -0600

    remove pointer to index.js

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const path = require('path');
 const { clog } = require('./middleware/clog');
-const api = require('./routes/index');
+// const api = require('./routes/index');
+const notesRouter = require('./routes/notes');
+const notes = require('./routes/notes');
 
 const PORT = process.env.PORT || 3001;
 
@@ -10,13 +12,15 @@ const app = express();
 app.use(clog);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
+// app.use('/api', api);
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
 });
+
+app.use('/notes', notesRouter);
 
 app.listen(PORT, () => {
     console.log('App listening at PORT ' + PORT)
