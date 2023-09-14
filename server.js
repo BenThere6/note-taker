5cdb6198bbf0dd838e63ab19c0b7e1414f9cc7d9
BenThere <benjaminbirdsall@icloud.com>
2023-09-14 15:08:35 -0600

    added get request to notes.html

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const { clog } = require('./middleware/clog');
-const api = requires('./routes/index.js')
+const api = require('./routes/index.js')
 
 const PORT = process.env.PORT || 3001;
 
@@ -16,6 +16,10 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
 });
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+})
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
