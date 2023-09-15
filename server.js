9a2316bf0a00ca6708f2848c17fb9ea471934044
BenThere <benjaminbirdsall@icloud.com>
2023-09-15 11:40:21 -0600

    clean up/ add comment

--- server.js
+++ server.js
@@ -7,24 +7,17 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+// Middleware
 app.use(clog);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', api);
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'))
-});
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
-})
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'))
-})
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 
-app.listen(PORT, () => {
-    console.log('App listening at PORT ' + PORT)
-});
+app.listen(PORT, () => console.log('App listening at PORT ' + PORT));
